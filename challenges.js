18e0ba0ce4ddecca9e8ee7ba5fabdaf78fe67345
mchljt <150992106+mchljt@users.noreply.github.com>
2025-11-10 11:06:51 +0800

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -9,7 +9,7 @@ const challenges = {
   "timeline1": "efb0b10c2766b690fcb879fb55ce1583a775ae15da536c21f9abe660f4fc5a79",
   "thor1": "1f3b650ab794a4414c85064ecf249f18c38b7da65377ac4919d71b1d64f84c95",
   "eztools1": "fe883582e52fc7adfe53a6aa4749870581da73e9ff9fb7840d18e3bef569a647",
-  "registry1": "b93258ffecb2e5be0487a0770af12e2c35328e6bde590f3e8b7d2cfd4ccc34bd",
+  "registry1": "36bde66f289a35683683b041c6d8f418a5f36607b547da25d00ad55891e80b88",
   "photorec1": "0e4de371ae8147bf7d336a5c2e7901b95bc15fcf5eb67855fc18104c3c36d6c0",
   // Add more if needed
 };
